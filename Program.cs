68aba8aeabe31cc99aaadb933b7b1212d24181b5
anu1820 <anurajpatil35@gmail.com>
2023-01-14 00:28:47 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@
     {
         public static void Main(string[] args) 
         {
+            LinkedHashMap<string, int> linked_hash_map = new LinkedHashMap<string, int>(5);
+
+            string Sentence = "to be or not to be";
+            string[] Words = Sentence.Split(" ");
+            foreach (string word in Words)
+            {
+                int Value = linked_hash_map.Get(word);
+                if (Value == default)
+                    Value = 1;
+                else Value++;
+                linked_hash_map.Add(word, Value);
+            }
+            int Frequency = linked_hash_map.Get("to");
             Console.WriteLine("Welcome to HashTable Program");
         }
     }
